fe57719308f71283ccd1fdbf72e9338f4e22f53e
kevalpansuriya <keval.pansuriya@hpe.com>
2024-02-26 17:38:46 +0530

    updated code enhancement

--- test_flipkart_login/locators.py
+++ test_flipkart_login/locators.py
@@ -1,9 +1,7 @@
 
 class FlipKartLocator:
     TITLE = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!"
-    LOGO = ('picture[title="Flipkart"] '
-            '[srcset="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images'
-            '/fkheaderlogo_exploreplus-44005d.svg"]')
+    LOGO = 'picture[title="Flipkart"]'
 
     SEARCH_BUTTON = '[placeholder="Search for Products, Brands and More"]'
 
--- test_flipkart_login/test_flipkart.py
+++ test_flipkart_login/test_flipkart.py
@@ -1,29 +1,39 @@
-import re
-from playwright.sync_api import Page, expect
 import logging
+import re
+
+from playwright.sync_api import Page, expect
 
 from test_flipkart_login.locators import FlipKartLocator
 
+log = logging.getLogger(__name__)
 url = "https://www.flipkart.com/"
 
 
-
-def test_has_title(page: Page):
+def test_has_title(page: Page) -> None:
+    """
+    Tests if the page has the title.
+    :param page:
+    :return:
+    """
     page.goto(url)
+    page.wait_for_url(url=url)
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile(FlipKartLocator.TITLE))
 
 
-def test_get_started_link(page: Page):
+def test_get_started_link(page: Page) -> None:
+    """
+    Tests if the page has the started link.
+    :param page:
+    :return:
+    """
     page.goto(url)
+    page.wait_for_url(url=url)
 
-    # Click the get started link.
+    # click the get started link.
     # page.get_by_role("link", name="Get started").click()
 
     # Expects page to have a heading with the name of Installation.
+    expect(page.locator(FlipKartLocator.LOGO)).to_be_visible()
     page.locator(FlipKartLocator.SEARCH_BUTTON).fill("toys")
-    logging.info(FlipKartLocator.LOGO)
-
-
-    #expect(page.locator(FlipKart.LOGO)).to_be_visible()
